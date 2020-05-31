1fa4a20c1ef8f270aebadd82143a555c6597269a
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-05-30 21:41:50 -0300

    [Imp] bcrypt in password

--- appcrud/controllers/UserController.js
+++ appcrud/controllers/UserController.js
@@ -1,10 +1,15 @@
 require('../database/conection_db.js');
 const User = require('../database/models/User.js');
+const bcrypt = require('bcrypt');
+
+function cryptPassword(password){
+    return bcrypt.hashSync(password, 10);
+}
 
 module.exports= { 
     async create(req,res) {
         const {username,password} = req.body;
-            await User.create({ username,password}).then(user => {
+            await User.create({ username,password:cryptPassword(password)}).then(user => {
                 console.log("User's auto-generated ID:", user.id);
             });
         return res.send('Sucess!');
