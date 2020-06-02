f16c05c288c0b1c0c377341041e5a269d239d2bf
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-06-01 23:07:44 -0300

    [Imp] generate table rows

--- appcrud/src/components/home/Home.jsx
+++ appcrud/src/components/home/Home.jsx
@@ -1,57 +1,80 @@
 import React, { Component } from 'react';
 import { Table } from 'evergreen-ui';
 import FormBar from '../templates/FormBar';
-import { render } from '@testing-library/react';
 
 class Home extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			idSelected:  '?',
-			usernameSelected:'',
-			data:'',
+			idSelected: '?',
+			usernameSelected: '',
+			data: [],
 		};
 		this.setData = this.setData.bind(this);
 		this.getData = this.getData.bind(this);
 		this.selectId = this.selectId.bind(this);
 		this.clearSelectionItem = this.clearSelectionItem.bind(this);
+		this.generateList = this.generateList.bind(this);
+		this.getData("");
 	}
-	setData(data){
-		this.setState({data});
+	setData(data) {
+		this.setState({ data });
 	}
+	/**
+	 * @description Return data of database from router /home port 3333
+	 * @function getData
+	 * @param {*} value 
+	 * @memberof Home
+	 */
 	getData(value) {
 		const urlencoded = new URLSearchParams();
-        urlencoded.append("username", value);
-        urlencoded.append("id", this.state.idSelected);
+		urlencoded.append("username", value);
+		urlencoded.append("id", this.state.idSelected);
 
-        const requestOptions = {
-            method: 'POST',
-            body: urlencoded,
-            redirect: 'follow'
-        };
+		const requestOptions = {
+			method: 'POST',
+			body: urlencoded,
+			redirect: 'follow'
+		};
 
-        fetch("http://localhost:3333/home", requestOptions)
-            .then(response => response.json())
+		fetch("http://localhost:3333/home", requestOptions)
+			.then(response => response.json())
 			.then(result => {
 				this.setData(result);
-				return console.log('reuslt:',result);
+				return console.log('reuslt:', result);
 			});
-			
-		  console.log('data:\n',this.state.data[0]);
+
+		console.log('data:\n', this.state.data[0]);
 	}
-	selectId(idSelected,usernameSelected){
-		this.setState({idSelected,usernameSelected});
+	selectId(idSelected, usernameSelected) {
+		this.setState({ idSelected, usernameSelected });
 	}
-	clearSelectionItem(){
-		this.setState({idSelected:'?'});
+	clearSelectionItem() {
+		this.setState({ idSelected: '?' });
 	}
+	generateList(){
+		return (
+
+			<Table.Body>
+				{this.state.data.map( userdata => {
+					return (
+						<Table.Row key={userdata.id} isSelectable onSelect={() => alert(userdata.username)}>
+							<Table.TextCell>{userdata.username}</Table.TextCell>
+							<Table.TextCell>{userdata.password}</Table.TextCell>
+						</Table.Row>
+					);
+				})}
+
+			</Table.Body>
+		)
+	};
 	render() {
 		return (
 			<React.Fragment>
 				<FormBar idSelected={'1'} usernameSelected={'icaro'} />
 
 				<Table>
-				<Table.SearchHeaderCell marginBottom={10} onChange={ value => this.getData(value) }/>
+					<Table.SearchHeaderCell marginBottom={10} onChange={value => this.getData(value)} />
 					<Table.Head>
 						<Table.TextHeaderCell>
 							Username
@@ -61,10 +84,7 @@ class Home extends Component {
     					</Table.TextHeaderCell>
 					</Table.Head>
 					<Table.Body>
-						<Table.Row key={'1'} isSelectable onSelect={() => alert('profile.name')}>
-							<Table.TextCell>{'profile.name'}</Table.TextCell>
-							<Table.TextCell>{'profile.ltv'}</Table.TextCell>
-						</Table.Row>
+					{this.generateList()}
 					</Table.Body>
 				</Table>
 			</React.Fragment>
